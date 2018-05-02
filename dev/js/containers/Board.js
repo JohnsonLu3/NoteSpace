4214abe961af8e2ff1a0103c4daf8062521d5df1
JohnsonLu3 <JohnsonLu94@gmail.com>
2018-05-02 13:59:02 -0400

    added note actions

--- dev/js/containers/Board.js
+++ dev/js/containers/Board.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
+import {addNote} from '../actions/NoteActions';
 /*
  * We need "if(!this.props.user)" because we set state to null by default
  * */
@@ -9,10 +11,21 @@ class Board extends Component {
      render() {
         return (
              <div id = "board">
-                    <i id="addButton" className="fa fa-plus-circle fa-3x"></i>
+                    <i id="addButton" onClick ={() => this.props.addNote()}className="fa fa-plus-circle fa-3x"></i>
             </div>
         );
     }
 }
 
-export default Board;
+// Actions
+function matchDispatchToProps(dispatch){
+    return bindActionCreators(
+        {
+            addNote: addNote
+        },
+         dispatch);
+}
+
+// We don't want to return the plain UserList (component) anymore, we want to return the smart Container
+//      > UserList is now aware of state and actions
+export default connect(matchDispatchToProps)(Board);
--- dev/js/containers/Note.js
+++ dev/js/containers/Note.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+import {editNote} from '../actions/NoteActions';
+import {removeNote} from '../actions/NoteActions';
+import {saveEdit} from '../actions/NoteActions';
+import {cancelEdit} from '../actions/NoteActions';
+
 
 class Note extends Component {
     
@@ -11,8 +18,8 @@ class Note extends Component {
         return (
                 <div className="Note" key={note.id} style={{backgroundColor: note.Color}}>
                     <div className = "editButtons">
-                        <i className="fa fa-check fa-2x" onClick=""></i>
-                        <i className="fa fa-ban fa-2x" onClick=""></i>
+                        <i className="fa fa-check fa-2x" onClick={() => this.props.saveEdit(note)}></i>
+                        <i className="fa fa-ban fa-2x" onClick={() => this.props.cancelEdit(note)}></i>
                     </div>
                     <div className="noteContents">
                         <textarea  rows="1" defaultValue={note.Header}></textarea>
@@ -30,8 +37,8 @@ class Note extends Component {
         return (
                 <div className="Note" key={note.id} style={{backgroundColor: note.Color}}>
                     <div className = "editButtons">
-                        <i className="fa fa-edit fa-2x" onClick=""></i>
-                        <i className="fa fa-trash fa-2x" onClick=""></i>
+                        <i className="fa fa-edit fa-2x" onClick={() => this.props.editNote(note)}></i>
+                        <i className="fa fa-trash fa-2x" onClick={() => this.props.removeNote(note)}></i>
                     </div>
                     <div className="noteContents">
                         <h2>{note.Header}</h2>
@@ -68,12 +75,18 @@ function mapStateToProps(state) {
     };
 }
 
-// Get actions and pass them as props to to UserList
-//      > now UserList has this.props.selectUser
-// function matchDispatchToProps(dispatch){
-//     return bindActionCreators({selectUser: selectUser}, dispatch);
-// }
+// Actions
+function matchDispatchToProps(dispatch){
+    return bindActionCreators(
+        {
+            editNote: editNote,
+            removeNote: removeNote,
+            saveEdit: saveEdit,
+            cancelEdit: cancelEdit
+        },
+         dispatch);
+}
 
 // We don't want to return the plain UserList (component) anymore, we want to return the smart Container
 //      > UserList is now aware of state and actions
-export default connect(mapStateToProps)(Note);
+export default connect(mapStateToProps,matchDispatchToProps)(Note);
